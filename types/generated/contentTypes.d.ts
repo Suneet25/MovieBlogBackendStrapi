54daca21f9ab515f72744d153f9e4ad3fa2b349d
Suneet25 <suneetpanigrahi2505@gmail.com>
2023-11-30 21:20:41 +0530

    added moviblog backend

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -361,6 +361,86 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiBlogBlog extends Schema.CollectionType {
+  collectionName: 'blogs';
+  info: {
+    singularName: 'blog';
+    pluralName: 'blogs';
+    displayName: 'Blogs';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Title: Attribute.String &
+      Attribute.Required &
+      Attribute.Unique &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    Category: Attribute.Enumeration<['Anime', 'Movie', 'Series']> &
+      Attribute.Required;
+    Summury: Attribute.Text;
+    isFeatured: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+    Thumbnail: Attribute.Media & Attribute.Required;
+    FeaturedImage: Attribute.Media;
+    Content: Attribute.String;
+    slug: Attribute.String & Attribute.Required;
+    Rating: Attribute.BigInteger & Attribute.Required;
+    Budget: Attribute.BigInteger;
+    Revenue: Attribute.BigInteger;
+    Status: Attribute.String;
+    OriginalLanguage: Attribute.String;
+    Runtime: Attribute.BigInteger;
+    ReleaseDate: Attribute.Date;
+    Country: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
+export interface ApiMovieBlogLandingMovieBlogLanding
+  extends Schema.CollectionType {
+  collectionName: 'movie_blog_landings';
+  info: {
+    singularName: 'movie-blog-landing';
+    pluralName: 'movie-blog-landings';
+    displayName: 'MovieBlogLanding';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Heading: Attribute.String;
+    Content: Attribute.Text;
+    Image: Attribute.Media;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::movie-blog-landing.movie-blog-landing',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::movie-blog-landing.movie-blog-landing',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -676,86 +756,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface ApiBlogBlog extends Schema.CollectionType {
-  collectionName: 'blogs';
-  info: {
-    singularName: 'blog';
-    pluralName: 'blogs';
-    displayName: 'Blogs';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    Title: Attribute.String &
-      Attribute.Required &
-      Attribute.Unique &
-      Attribute.SetMinMaxLength<{
-        minLength: 3;
-        maxLength: 255;
-      }>;
-    Category: Attribute.Enumeration<['Anime', 'Movie', 'Series']> &
-      Attribute.Required;
-    Summury: Attribute.Text;
-    isFeatured: Attribute.Boolean &
-      Attribute.Required &
-      Attribute.DefaultTo<false>;
-    Thumbnail: Attribute.Media & Attribute.Required;
-    FeaturedImage: Attribute.Media;
-    Content: Attribute.String;
-    slug: Attribute.String & Attribute.Required;
-    Rating: Attribute.BigInteger & Attribute.Required;
-    Budget: Attribute.BigInteger;
-    Revenue: Attribute.BigInteger;
-    Status: Attribute.String;
-    OriginalLanguage: Attribute.String;
-    Runtime: Attribute.BigInteger;
-    ReleaseDate: Attribute.Date;
-    Country: Attribute.String;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-  };
-}
-
-export interface ApiMovieBlogLandingMovieBlogLanding
-  extends Schema.CollectionType {
-  collectionName: 'movie_blog_landings';
-  info: {
-    singularName: 'movie-blog-landing';
-    pluralName: 'movie-blog-landings';
-    displayName: 'MovieBlogLanding';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    Heading: Attribute.String;
-    Content: Attribute.Text;
-    Image: Attribute.Media;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::movie-blog-landing.movie-blog-landing',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::movie-blog-landing.movie-blog-landing',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/strapi' {
   export module Shared {
     export interface ContentTypes {
@@ -766,14 +766,14 @@ declare module '@strapi/strapi' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::blog.blog': ApiBlogBlog;
+      'api::movie-blog-landing.movie-blog-landing': ApiMovieBlogLandingMovieBlogLanding;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'api::blog.blog': ApiBlogBlog;
-      'api::movie-blog-landing.movie-blog-landing': ApiMovieBlogLandingMovieBlogLanding;
     }
   }
 }
